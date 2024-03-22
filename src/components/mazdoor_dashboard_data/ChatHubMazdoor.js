4a3c20d964708d44812d5ab853de7cb4270592c6
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-03-22 12:47:42 +0500

    jobs page design

--- src/components/mazdoor_dashboard_data/ChatHubMazdoor.js
+++ src/components/mazdoor_dashboard_data/ChatHubMazdoor.js
@@ -42,10 +42,6 @@ export default function ChatHubMazdoor() {
   ];
   return (
     <div className="w-full bg-white">
-      <div className="bg-gray-50 dark:bg-gray-700 px-5 py-5">
-        <p className="text-white">Chat Hub</p>
-      </div>
-
       <section className="w-full">
         <div className="w-full flex flex-col justify-center items-center sm:flex-row sm:justify-start sm:items-start">
           <div className="md:w-1/4 w-full bg-gray-300 h-screen flex flex-col justify-start items-center">
@@ -133,7 +129,7 @@ export default function ChatHubMazdoor() {
                     />
                     <div className="flex flex-col gap-1 w-full max-w-[320px]">
                       <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                        <span className="text-sm font-semibold text-gray-900 dark:text-black">
                           Bonnie Green
                         </span>
                         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
@@ -142,7 +138,6 @@ export default function ChatHubMazdoor() {
                       </div>
                       <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                         <p className="text-sm font-normal text-gray-900 dark:text-white">
-                          {" "}
                           That's awesome. I think our users will really
                           appreciate the improvements.
                         </p>
@@ -151,6 +146,83 @@ export default function ChatHubMazdoor() {
                     </div>
                   
                   </div>
+                  <div className="flex items-start gap-2.5 py-3 px-3">
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=1504"
+                      alt="user image"
+                    />
+                    <div className="flex flex-col gap-1 w-full max-w-[320px]">
+                      <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                        <span className="text-sm font-semibold text-gray-900 dark:text-black">
+                          Bonnie Green
+                        </span>
+                        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                          11:46
+                        </span>
+                      </div>
+                      <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                        <p className="text-sm font-normal text-gray-900 dark:text-white">
+                          That's awesome. I think our users will really
+                          appreciate the improvements.
+                        </p>
+                      </div>
+                    
+                    </div>
+                  
+                  </div>
+                  <div className="flex items-start gap-2.5 py-3 px-3">
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=1504"
+                      alt="user image"
+                    />
+                    <div className="flex flex-col gap-1 w-full max-w-[320px]">
+                      <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                        <span className="text-sm font-semibold text-gray-900 dark:text-black">
+                          Bonnie Green
+                        </span>
+                        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                          11:46
+                        </span>
+                      </div>
+                      <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                        <p className="text-sm font-normal text-gray-900 dark:text-white">
+                          That's awesome. I think our users will really
+                          appreciate the improvements.
+                        </p>
+                      </div>
+                    
+                    </div>
+                  
+                  </div>
+
+                  <div className="flex justify-end items-start gap-2.5 py-3 px-3">
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=1504"
+                      alt="user image"
+                    />
+                    <div className="flex flex-col gap-1 w-full max-w-[320px]">
+                      <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                        <span className="text-sm font-semibold text-gray-900 dark:text-black">
+                          Bonnie Green
+                        </span>
+                        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                          11:46
+                        </span>
+                      </div>
+                      <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-900">
+                        <p className="text-sm font-normal text-gray-900 dark:text-white">
+                          That's awesome. I think our users will really
+                          appreciate the improvements.
+                        </p>
+                      </div>
+                    
+                    </div>
+                  
+                  </div>
+                  
                 </div>
 
                 <div className="w-full py-5 bg-slate-200 px-3 flex flex-row justify-between items-center">
@@ -159,7 +231,7 @@ export default function ChatHubMazdoor() {
                       type="text"
                       id="message"
                       className="block w-full py-3 px-2 ps-10 text-sm text-gray-900 border border-gray-600 rounded-lg bg-gray-50 focus:ring-blue-500  focus:border-blue-500 dark:bg-gray-400 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="enter message here ..."
+                      placeholder="Enter message here ..."
                       required=""
                     />
                   </form>
