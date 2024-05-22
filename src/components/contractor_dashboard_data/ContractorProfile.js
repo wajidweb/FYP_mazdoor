0612e10dd71e609bbbd77d47aa8381422303f3aa
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-05-23 01:37:40 +0500

    users profile page validations

--- src/components/contractor_dashboard_data/ContractorProfile.js
+++ src/components/contractor_dashboard_data/ContractorProfile.js
@@ -16,6 +16,89 @@ export default function ContractorProfile() {
     }
   };
 
+
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    companyName: "",
+    contactNumber: "",
+    city: "",
+    address: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    companyName: "",
+    contactNumber: "",
+    city: "",
+    address: "",
+  });
+
+  const formValidation = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (!formData.name || formData.name?.trim() === "") {
+      newErrors.name = "Please enter name here";
+      valid = false;
+    } else {
+      newErrors.name = "";
+    }
+    if (!formData.companyName || formData.companyName?.trim() === "") {
+      newErrors.companyName = "Please enter company name here";
+      valid = false;
+    } else {
+      newErrors.companyName = "";
+    }
+    if (!formData.contactNumber || formData.contactNumber?.trim() === "") {
+      newErrors.contactNumber = "Please enter contact number here";
+      valid = false;
+    } else {
+      newErrors.contactNumber = "";
+    }
+
+    if (!formData.city || formData.city?.trim() === "") {
+      newErrors.city = "Please enter your city";
+      valid = false;
+    } else {
+      newErrors.city = "";
+    }
+    if (!formData.address || formData.address?.trim() === "") {
+      newErrors.address = "Please enter your address";
+      valid = false;
+    } else {
+      newErrors.address = "";
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleFormInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: "",
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formValidation()) {
+      console.log("success", formData);
+    }
+    else{
+      console.log("unsuccess")
+    }
+  };
+
+  console.log("formdata is", formData);
+
+
   return (
     <div className="w-full bg-white">
       <div className="bg-gray-50 dark:bg-gray-700 px-5 py-5">
@@ -23,7 +106,7 @@ export default function ContractorProfile() {
       </div>
 
       <section className="w-full  flex justify-center items-center  py-2">
-        <form className="w-2/4 mx-auto ">
+        <form className="w-2/4 mx-auto " onSubmit={handleSubmit}>
           <div className="flex justify-center items-center mx-auto p-6">
             <div className="mb-4">
               <input
@@ -54,18 +137,21 @@ export default function ContractorProfile() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                name="fullName"
-                id="fullName"
+                name="name"
+                id="name"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                value={formData?.name || ""}
+                onChange={handleFormInputChange}
               />
               <label
-                htmlFor="fullName"
+                htmlFor="name"
                 className="peer-focus:font-medium absolute text-sm text-gray-600 dark:text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Full Name
               </label>
+              {errors.name && <div className="text-red-400">{errors.name}</div>}
             </div>
             <div className="relative z-0 w-full mb-5 group">
               <input
@@ -75,6 +161,8 @@ export default function ContractorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                value={formData?.companyName || ""}
+                onChange={handleFormInputChange}
               />
               <label
                 htmlFor="companyName"
@@ -82,6 +170,7 @@ export default function ContractorProfile() {
               >
                 Company Name (if not - then enter your name)
               </label>
+              {errors.companyName && <div className="text-red-400">{errors.companyName}</div>}
             </div>
           </div>
           <div className="grid md:grid-cols-2 md:gap-6">
@@ -94,6 +183,8 @@ export default function ContractorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                value={formData?.contactNumber || ""}
+                onChange={handleFormInputChange}
               />
               <label
                 htmlFor="contactNumber"
@@ -101,6 +192,7 @@ export default function ContractorProfile() {
               >
                 Phone number (123-456-7890)
               </label>
+              {errors.contactNumber && <div className="text-red-400">{errors.contactNumber}</div>}
             </div>
             <div className="relative z-0 w-full mb-5 group">
               <input
@@ -110,6 +202,8 @@ export default function ContractorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                value={formData?.city || ""}
+                onChange={handleFormInputChange}
               />
               <label
                 htmlFor="city"
@@ -117,6 +211,7 @@ export default function ContractorProfile() {
               >
                 City
               </label>
+              {errors.city && <div className="text-red-400">{errors.city}</div>}
             </div>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -131,7 +226,11 @@ export default function ContractorProfile() {
               rows="3"
               className="block p-2.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your address..."
+              value={formData?.address || ""}
+              name = "address"
+              onChange={handleFormInputChange}
             ></textarea>
+              {errors.address && <div className="text-red-400">{errors.address}</div>}
           </div>
          
           <div className="flex justify-center items-center w-full">
--- src/components/mazdoor_dashboard_data/MazdoorProfile.js
+++ src/components/mazdoor_dashboard_data/MazdoorProfile.js
@@ -16,6 +16,91 @@ export default function MazdoorProfile() {
     }
   };
 
+  const [formData, setFormData] = useState({
+    name: "",
+    profession: "",
+    contactNumber: "",
+    city: "",
+    address: "",
+    description: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    profession: "",
+    contactNumber: "",
+    city: "",
+    address: "",
+    description: "",
+  });
+
+  const formValidation = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (!formData.name || formData.name?.trim() === "") {
+      newErrors.name = "Please enter name here";
+      valid = false;
+    } else {
+      newErrors.name = "";
+    }
+    if (!formData.profession || formData.profession?.trim() === "") {
+      newErrors.profession = "Please enter profession here";
+      valid = false;
+    } else {
+      newErrors.profession = "";
+    }
+    if (!formData.contactNumber || formData.contactNumber?.trim() === "") {
+      newErrors.contactNumber = "Please enter contact number here";
+      valid = false;
+    } else {
+      newErrors.contactNumber = "";
+    }
+
+    if (!formData.city || formData.city?.trim() === "") {
+      newErrors.city = "Please enter your city";
+      valid = false;
+    } else {
+      newErrors.city = "";
+    }
+    if (!formData.address || formData.address?.trim() === "") {
+      newErrors.address = "Please enter your address";
+      valid = false;
+    } else {
+      newErrors.address = "";
+    }
+    if (!formData.description || formData.description?.trim() === "") {
+      newErrors.description = "Please enter your description";
+      valid = false;
+    } else {
+      newErrors.description = "";
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleFormInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: "",
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formValidation()) {
+      console.log(formData);
+    }
+  };
+
+  console.log("formdata is", formData);
+
   return (
     <div className="w-full bg-white">
       <div className="bg-gray-50 dark:bg-gray-700 px-5 py-5">
@@ -23,7 +108,7 @@ export default function MazdoorProfile() {
       </div>
 
       <section className="w-full  flex justify-center items-center  py-2">
-        <form className="w-2/4 mx-auto ">
+        <form className="w-2/4 mx-auto" onSubmit={handleSubmit}>
           <div className="flex justify-center items-center mx-auto p-6">
             <div className="mb-4">
               <input
@@ -54,19 +139,23 @@ export default function MazdoorProfile() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                name="fullName"
-                id="fullName"
+                name="name"
+                value={formData?.name || ""}
+                id="name"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                onChange={handleFormInputChange}
               />
               <label
-                htmlFor="fullName"
+                htmlFor="name"
                 className="peer-focus:font-medium absolute text-sm text-gray-600 dark:text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Full Name
               </label>
+              {errors.name && <div className="text-red-400">{errors.name}</div>}
             </div>
+
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
@@ -75,6 +164,8 @@ export default function MazdoorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                onChange={handleFormInputChange}
+                value={formData?.profession || ""}
               />
               <label
                 htmlFor="profession"
@@ -82,6 +173,7 @@ export default function MazdoorProfile() {
               >
                 Profession
               </label>
+              {errors.profession && <div className="text-red-400">{errors.profession}</div>}
             </div>
           </div>
           <div className="grid md:grid-cols-2 md:gap-6">
@@ -94,6 +186,8 @@ export default function MazdoorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                onChange={handleFormInputChange}
+                value={formData?.contactNumber || ""}
               />
               <label
                 htmlFor="contactNumber"
@@ -101,6 +195,7 @@ export default function MazdoorProfile() {
               >
                 Phone number (123-456-7890)
               </label>
+              {errors.contactNumber && <div className="text-red-400">{errors.contactNumber}</div>}
             </div>
             <div className="relative z-0 w-full mb-5 group">
               <input
@@ -110,6 +205,8 @@ export default function MazdoorProfile() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required=""
+                onChange={handleFormInputChange}
+                value={formData?.city || ""}
               />
               <label
                 htmlFor="city"
@@ -117,6 +214,7 @@ export default function MazdoorProfile() {
               >
                 City
               </label>
+              {errors.city && <div className="text-red-400">{errors.city}</div>}
             </div>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -127,11 +225,14 @@ export default function MazdoorProfile() {
               Address
             </label>
             <textarea
+              onChange={handleFormInputChange}
+              name="address"
               id="address"
               rows="3"
               className="block p-2.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your address..."
             ></textarea>
+              {errors.address && <div className="text-red-400">{errors.address}</div>}
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <label
@@ -141,11 +242,14 @@ export default function MazdoorProfile() {
               Professional Description
             </label>
             <textarea
+              name="description"
+              onChange={handleFormInputChange}
               id="professionalDescription"
               rows="3"
               className="block p-2.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Description..."
             ></textarea>
+              {errors.description && <div className="text-red-400">{errors.description}</div>}
           </div>
           <div className="flex justify-center items-center w-full">
             <button
